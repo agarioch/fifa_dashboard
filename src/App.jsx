6ac26dcfe5a5612d0ecccc68b665f92e3c6f3002
agarioch <alistairgarioch@gmail.com>
2020-07-11 18:01:48 +0100

    Added histogram showing number of players by overall rating bin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types'
 import * as d3 from 'd3'
 
 import './App.css';
+import Histogram from "./Histogram";
+
+const overallAccessor = d => parseFloat(d["Overall"]) || 0;
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    d3.csv("../data/data.csv").then(dataset => setData(dataset))
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+    <h1>FIFA Ratings</h1>
+      <div className="App__charts">
+        <Histogram
+          data={data}
+          xAccessor={overallAccessor}
+          label={"Overall"}
+        />
+      </div>
     </div>
   );
 }
