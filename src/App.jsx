7796c255d54a4ed70f5361b60214d184ff3e9cb6
agarioch <alistairgarioch@gmail.com>
2020-07-11 11:53:15 +0100

    Blank create react app template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import * as d3 from 'd3'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
